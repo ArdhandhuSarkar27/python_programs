7ee8c2172e0e317e6e7f3f1d317b3b2e4dba1dc4
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-04-09 18:47:03 +0530

    Add files via upload

---
+++ float_avg_num.py
@@ -0,0 +1,9 @@
+a = float(input("Enter First Number:"))
+b = float(input("Enter Second Number:"))
+avg = (a+b)/2
+print(avg)
+
+#METHOD 2 of finding avg using only print
+
+print("avgerage = ",(a+b)/2) 
+
