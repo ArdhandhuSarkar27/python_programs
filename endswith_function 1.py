6aa22ddcc891a8b56ea19f7f4c9497cfa6013d7a
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-04-13 18:41:38 +0530

    Add files via upload

---
+++ endswith_function 1.py
@@ -0,0 +1,8 @@
+str = "This is a python program for endswith() function"
+print(str.endswith("()"))
+print(str.endswith("ion"))
+
+#method 2
+
+str = input("Enter the string you want:\n")
+print(str.endswith(input("Enter the string to use the endswith function:\n")))
