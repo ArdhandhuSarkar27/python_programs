b655d881d6b6f3226ff175ef165300b68e69da72
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-04-15 18:47:38 +0530

    Add files via upload

---
+++ capitalize_function.py
@@ -0,0 +1,9 @@
+str = "this is the capatilize function"
+str = str.capitalize()
+print(str)
+
+#METHOD 2 BY TAKING INPUT FROM THE USER
+
+str = input("Enter the string you want to capitalize:\n")
+str = str.capitalize()
+print(str)
