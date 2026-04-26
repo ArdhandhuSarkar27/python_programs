f160a83b5121817a0b1094180d2d2e53afe613f1
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-04-26 11:59:26 +0530

    Add files via upload

---
+++ append_sort_reverse.py
@@ -0,0 +1,7 @@
+List = [2,1,3]
+List.append(4)#adds a new item at the last of the list
+print(List)
+List.sort()#sorts the order from ascending to decending order
+print(List)
+List.sort(reverse = True)#sorts the order from decending to ascening order
+print(List)
