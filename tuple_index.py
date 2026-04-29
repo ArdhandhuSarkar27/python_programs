bcab590fa1cf8302c74ccfed838a2c262140ccdf
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-04-29 19:05:51 +0530

    Add files via upload

---
+++ tuple_index.py
@@ -0,0 +1,14 @@
+#To treat "TUPLE" as only a "TUPLE" and not as a string we use comma(,) at the last inside paranthesis()
+
+tuple = ("hello",)
+print(tuple)
+print(type(tuple))
+
+'''"TUPLE" indexing is same as normal indexing method'''
+
+tuple = (9,1,1,0,6,8,6,2,7,4)
+print(tuple[1:7])
+print(tuple[:10])
+print(tuple[0:])
+
+
