1caea48e66ff5cf52f1c9d3863e97fe0f18df6b3
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-05-03 11:27:50 +0530

    Add files via upload

---
+++ dictionary_set.py
@@ -0,0 +1,15 @@
+info = {
+    "name":"Krishna",
+    "subjects":["python","C","Java"],
+    "topics":("dict","set"),
+    "age":19,
+    "is_adult": True,
+    "marks": 96.5
+}
+print(info["name"])
+print(info["subjects"])
+print(info["topics"])
+print(info["age"])
+print(info["is_adult"])
+print(info["marks"])
+print(info)
