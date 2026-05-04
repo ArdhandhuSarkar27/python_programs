c238124a4dd12926a671c9284ef543cdb10c2244
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-05-04 19:36:50 +0530

    Add files via upload

---
+++ nested_dictionary.py
@@ -0,0 +1,10 @@
+student = {
+    "name": "Radha",
+    "subjects":{
+        "phy":97,
+        "chem":98,
+        "math":95
+    }
+}
+print(student["subjects"]["chem"])
+print(student["subjects"])
