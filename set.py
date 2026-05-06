a56042335c29328a3dd3a1c925b634f4fe1ca135
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-05-06 09:29:36 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,4 @@
+#Sets are immutable and when the sets are printed there is no order in printing and repeated texts are not printed 
+coll = {1,2,3,4,"hello","world",4,"world"}
+print(coll)
+print(type(coll))
