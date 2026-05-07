702da540b2857d2aea66baeaef481886de05e5b2
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-05-07 19:01:34 +0530

    Add files via upload

---
+++ dictionary.py
@@ -0,0 +1,8 @@
+dict = {
+    "cat":"A small animal",
+    "table":["A piece of furniture",
+             "List of fact and figures"] #There are two values for table so to store them without any further error "LISTS" can be used 
+}
+print(dict)
+print(dict["cat"])
+print(dict["table"])
