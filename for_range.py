3adc4aa8513b367318c31c266e3cde8a9cc95535
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-06-02 09:24:41 +0530

    Add files via upload

---
+++ for_range.py
@@ -0,0 +1,7 @@
+#for loop range for start stop and step size
+
+for sss in range(2,10,2):
+ '''Here the first number "2" is the starting number the number "10"
+is the range and the last number "2" is the difference between the starting 
+and range which will be "2"''' 
+ print(sss)
