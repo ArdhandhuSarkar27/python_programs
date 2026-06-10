24e202597ed7cf2e2f4367c62b2f89dca7cf8f6d
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-06-10 06:28:16 +0530

    Add files via upload

---
+++ intro_function.py
@@ -0,0 +1,11 @@
+#Function in Python(Definition)
+'''It is a block of statement to perform a specific task'''
+
+#syntax
+
+'''def func_name(parameter1,parameter2):
+        return val
+    func_name(arg1,arg2)'''
+
+#function definition(the one which are in paranthesis which is(parameter1,parameter2)in the first line of syntax)
+#function call(the one which are in paranthesis which is(arg1,arg2)in the last line of syntax)
