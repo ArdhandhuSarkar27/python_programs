21bc84fca0a138856ea93d1d83c1ae56237a1209
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-06-16 14:33:17 +0530

    Add files via upload

---
+++ def_default_arg.py
@@ -0,0 +1,7 @@
+'''Default argument Definition
+The arguments in are within the paranthesis is already assigned the values(constant values)'''
+
+def cal_prod(a=1,b=2):
+    print(a*b)
+    return a*b
+cal_prod()
