c189f22e95fdc43e5ad1ae41d0ed1cc5f672ba6d
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-06-17 20:16:55 +0530

    Add files via upload

---
+++ practiceQ6.py
@@ -0,0 +1,8 @@
+'''WAP to print length of a list(list is the parameter)'''
+
+cities = ["Barsana","Vrindavan","Kedarnath"]
+Gods = ["Radha Rani","Krishna","Mahadev"]
+def print_len(list):
+    print(len(list))
+print_len(cities)
+print_len(Gods)
