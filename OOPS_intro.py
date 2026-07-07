82106b766eaab938ca89c2512c9916818f742bc5
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-07-07 16:04:38 +0530

    Add files via upload

---
+++ OOPS_intro.py
@@ -0,0 +1,19 @@
+#OOPS 
+
+#OOPS Definition - ""To map with real world scenarious, we started using onjects in code
+# This is called OOPS""
+
+#CLASS Definition - ""Class is a blueprint for creating objects""
+
+#Creating class
+
+class Student:
+    name = "Krishna"
+
+#creating objects(instance)
+
+s1 = Student()
+print(s1.name)
+
+#o/p will be Krishna
+
