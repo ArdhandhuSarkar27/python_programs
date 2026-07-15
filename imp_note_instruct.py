2055ddeabe7fde55b6e414b61ac2f5d6eefabb3e
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-07-15 15:04:27 +0530

    Add files via upload

---
+++ imp_note_instruct.py
@@ -0,0 +1,5 @@
+#IMPORTANT NOTE OF OOPS CONCEPT CODE:
+
+"""1. Python by default constructs a constructor for us
+   2. But if we need our own constructor with our wished 
+      name in it then we use the previous question like type which is  def2_function.py"""
