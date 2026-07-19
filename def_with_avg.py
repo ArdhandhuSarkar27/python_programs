9e6b9c9f0c6878c61ee39f5e971bc97530e9b7c1
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-07-19 11:50:36 +0530

    Add files via upload

---
+++ def_with_avg.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,name,marks):
+        self.name = name
+        self.marks = marks
+    def get_avg(self):
+        sum = 0
+        for val in self.marks:
+            sum+=val
+        print("Hi",self.name,"Your avg score is",sum/3)
+s1 = Student("Shyam Sundar Sri Radhey",[99,98,99])
+s1.get_avg()
+s1.name = "Radhey Krishn"
+s1.get_avg()
