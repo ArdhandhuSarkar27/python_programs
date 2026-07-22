b2802c3c1fa309de44811fe3cf9c5ef4a1a19789
Ardhandhu Sarkar <radhe911020@gmail.com>
2026-07-22 14:01:18 +0530

    Add files via upload

---
+++ static_method_definition.py
@@ -0,0 +1,3 @@
+"""If you want to define a static method, you can use the @staticmethod decorator.
+ A static method does not receive an implicit first argument (like self or cls).
+   It behaves like a regular function but belongs to the class's namespace. @staticmethod can be called on the class itself or on an instance of the class."""
